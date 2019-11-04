ab21fc8930ea095235ee36b5a838cf4850f428a8
setsuotokohorikawa <ralralph@gmail.com>
2019-11-04 16:57:34 +0900

    CSVupload機能実装

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+  require 'csv'
+
+  def self.import(file)
+    CSV.foreach(file.path, headers: true) do |row|
+      # IDが見つかれば、レコードを呼び出し、見つかれなければ、新しく作成
+      user = find_by(id: row["id"]) || new
+      # CSVからデータを取得し、設定する
+      user.attributes = row.to_hash.slice(*updatable_attributes)
+      # 保存する
+      user.save
+    end
+  end
+
+  def self.updatable_attributes
+    ["id", "name", "age"]
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,19 @@
+User.create!(
+  [
+    {
+      id: 1,
+      name: 'hoge1',
+      age: 24
+    },
+    {
+      id: 2,
+      name: 'hoge2',
+      age: 31
+    },
+    {
+      id: 3,
+      name: 'hoge3',
+      age: 30
+    }
+  ]
+)
